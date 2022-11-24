7ee58cb871d3dfeb922683221fa810a582790804
michael seaman <mseaman26@gmail.com>
2022-11-23 19:23:36 -0800

    controllers for create user and delete user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, Thought } = require('../models');
 
 module.exports = {
     //get all users
@@ -7,7 +7,7 @@ module.exports = {
         .then((users) => res.json(users))
         .catch((err) => res.status(500).json(err))
     },
-    // get single users
+    // get single user
     getSingleUser(req, res) {
         User.findOne({ _id: req.params.userId})
         .select('-__v')
@@ -18,7 +18,28 @@ module.exports = {
                 res.json(user)
             }
         })
-    }
+    },
 
-    
+    //create single user
+    createUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err))
+    },
+
+    // delete single user
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId})
+            .then((user) => {
+                if(!user){
+                    res.status(404).json({ message: 'cannot fine user'})
+                }else{
+                    Thought.deleteMany({ _id: {
+                        $in:user.thoughts
+                    }})
+                }
+            })
+            .then(() => res.json({ message: 'user and all associated thoughts have been deleted!'}))
+            .catch(() => res.status(500).json(err))
+    }
 }
