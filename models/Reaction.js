09e6be7603a9546f8a8a882be4af2a2c00f1cc72
michael seaman <mseaman26@gmail.com>
2022-11-23 17:42:03 -0800

    first draft of Reaction.js

---
+++ models/Reaction.js
@@ -0,0 +1,24 @@
+const { Schema } = require('mongoose');
+
+const reactionSchema = new Schema ({
+    reactionID: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        //TODO: figure out time formatting on query
+    },
+})
+
+module.exports = reactionSchema
