d79e98c2f6f641dd63c60172e152a65980646704
michael seaman <mseaman26@gmail.com>
2022-11-23 20:10:18 -0800

    initial userRoutes setup

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,17 @@
 const router = require('express').Router();
+
+const { 
+    getUsers, 
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/userController')
+
+// /api/users
+router.route('/').get(getUsers).post(createUser)
+
+// /apu//users/:userId
+router.router('/:userId').get(getSingleUser).put(updateUser)
+    .delete(deleteUser)
+module.exports = router;
